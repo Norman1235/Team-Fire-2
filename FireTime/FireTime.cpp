07ab9a2a4779df10c69dfc353f2f87892251b637
Norman1235 <NPrice@student.fullsail.edu>
2026-08-12 16:18:38 -0400

    Blank

--- FireTime/FireTime.cpp
+++ FireTime/FireTime.cpp
@@ -1,28 +1,39 @@
-// FireTime.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <string>
 #include <iostream>
+bool IsPrime(int N);
 
 int main()
 {
 	std::string stringOut = "";
+	int currentNumber = 1; //We start printing at the number 1
+	int endingPoint = 101;
 
-	for (int currentNumber = 1; currentNumber < 101; currentNumber++)
+
+	for (currentNumber; currentNumber <= endingPoint; currentNumber++)
+
 	{
 		stringOut = std::to_string(currentNumber) + " ";
 
-		if (currentNumber % 3 == 0)
+		if (currentNumber % 3 == 0) //Fizz prints if currentNmber is divisable by 3
 		{
 			stringOut += "Fizz";
 
 		}
 
+		if (IsPrime(currentNumber) == true)
+		{
+			std::cout << "P\n";
+		}
 
-		if (currentNumber % 5 == 0)
+		if (currentNumber % 5 == 0) //Buzz prints if currentNmber is divisable by 5
 		{
 			stringOut += "Buzz";
 
+		}
+
+		if (currentNumber % 10 == 0)
+		{
+			stringOut += " 10x";
 		}
 
 		std::cout << stringOut << "\n";
@@ -32,13 +43,21 @@ int main()
 	}
 
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool IsPrime(int N)
+{
+	if (N <= 1) return false;
+
+	if (N <= 3) return false;
+
+	if (N % 2 == 0 || N % 3 == 0) return false;
+
+	for (int i = 5; i * i <= N; i += 6)
+	{
+		if (N % i == 0 || N % (i + 2) == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
